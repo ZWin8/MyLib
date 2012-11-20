27cab559dc58e0464e59599aff7f4ccf178d07f5
ZNATZ <Z.NAT.CN@GMAIL.COM>
2012-11-20 17:10:48 +0900

    A working library of coloritemctrl

---
+++ App1/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using NATControl;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace App1
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            IEnumerable<PropertyInfo> properties = typeof(Colors).GetTypeInfo().DeclaredProperties;
+            foreach (PropertyInfo property in properties)
+            { 
+                Color clr = (Color)property.GetValue(null); 
+                ColorItemCtrl clrItem = new ColorItemCtrl(property.Name, clr); 
+                MainStackPanel.Children.Add(clrItem); 
+            } 
+        }
+    }
+}
---
+++ NATControl/ColorItemCtrl.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace NATControl
+{
+    public sealed partial class ColorItemCtrl : UserControl
+    {
+        public ColorItemCtrl(string clrname, Color clr)
+        {
+            this.InitializeComponent();
+            txtblkName.Text = clrname;
+            txtblkRgb.Text  = String.Format("{0:X2} {1:X2} {2:X2}", clr.R, clr.G, clr.B);
+            rectangle.Fill  = new SolidColorBrush(clr);
+        }
+    }
+}
